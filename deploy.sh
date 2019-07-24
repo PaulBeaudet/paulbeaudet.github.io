c8d5ae895bfed3b09e75647346d4b2bc7c73f596
Paul Beaudet <inof8or@gmail.com>
2019-07-24 15:24:25 -0400

    remove discuss and google analytics

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
 git commit -m "$1"
-JEKYLL_ENV=production jekyll build
+JEKYLL_ENV=production bundle exec jekyll build
 aws s3 sync _site/ s3://paul.deabute.com --delete
 git push
