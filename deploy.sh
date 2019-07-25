c8dbcf902a86a7e8a866b654eea40dec808ff7e7
Paul Beaudet <inof8or@gmail.com>
2019-07-25 12:14:05 -0400

    exclude scripts

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 git add .
 git commit -m "$1"
 JEKYLL_ENV=production bundle exec jekyll build
-aws s3 sync _site/ s3://paul.deabute.com --delete
+aws s3 sync _site/ s3://paul.deabute.com --delete --exclude "*.sh"
 git push
