b55e8d507e05df1eb84070c13d269c516b4b68cb
Paul Beaudet <inof8or@gmail.com>
2020-06-04 16:32:23 -0400

    known good spot

--- exportToGatsby.sh
+++ exportToGatsby.sh
@@ -21,20 +21,36 @@ for post in _posts/*; do
     postToMatch="$(tr [A-Z] [a-z] <<< "$hyphensplitThree")"
     echo $postToMatch
     matchFound=false
+    matchedPicDir=''
     for picDirs in assets/img/hiking/*; do
         pics="$(tr [A-Z] [a-z] <<< "$picDirs")"
         if [[ "$pics" == *"$postToMatch"* ]]; then
             echo "match-> $picDirs"
             matchFound=true
+            matchedPicDir=$picDirs
             # newFolder=$HOME/development/react/paul_deabute_com_blog/content/blog2/$slashsplitOne}
             newFolder=$HOME/Downloads/blog2/$slashsplitOne
-            mkdir -p $newFolder
-            cp $post $newFolder/$slashsplitOne.md
-            cp $picDirs/* $newFolder/
+            # mkdir -p $newFolder
+            # cp $post $newFolder/$slashsplitOne.md
+            # cp $picDirs/* $newFolder/
             break
         fi
     done
     if [ $matchFound == false ]; then
+        # find linked pictures manually and copy them a folder made for this md post
         echo "No match found"
+    else
+        # Reword every image link in markdown to point internally (./imagename)
+        while read line; do
+            # Find line with ![]
+            if [[ "![" == "$line"* ]]; then
+                echo $line
+            fi
+        done < $post
     fi
+<<COMMENT
+    mkdir -p reblog
+    cp $post reblog/$slashsplitOne.md
+    grep -Fn '![' $copiedPost | sed "s:(.*):(./$slashsplitOne/:g"
+COMMENT
 done
